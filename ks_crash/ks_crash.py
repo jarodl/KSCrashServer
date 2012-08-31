fb1379f302618827c6b1f1a7de6512f83d311529
Jarod Luebbert <jarodluebbert@gmail.com>
2012-08-30 17:11:34 -0700

    a basic (shitty) redis store

--- ks_crash/ks_crash.py
+++ ks_crash/ks_crash.py
@@ -1,7 +1,9 @@
 import os
+import json
 
 from flask import Flask
 from flask import render_template, redirect, url_for, request
+from models.report import Report
 
 def create_app():
     return Flask(__name__)
@@ -12,8 +14,21 @@ app.config.from_pyfile("config.py")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    reports = Report.all()
+    return render_template('index.html', reports=reports)
+
+@app.route('/api/crashes/', methods=['POST'])
+def crashes():
+    if request.method == 'POST':
+        json_file = request.files['reports']
+        reports = json.loads(json_file.read())
+        for report_dict in reports:
+            crash_id = report_dict['crash_id']
+            report = Report(crash_id, report_dict)
+            report.save()
+        return reports.__str__()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
---
+++ ks_crash/models/report.py
@@ -0,0 +1,83 @@
+from redis import Redis
+import json
+
+class BaseItem(object):
+    _redis = Redis()
+
+    def __init__(self, ident, object_id=None):
+        self.name = self.__class__.__name__
+        self.ident = ident
+        self.object_id = object_id
+        self.redis_key = self.make_key(self.name, self.ident)
+
+    @classmethod
+    def all(cls):
+        identifiers = cls._redis.smembers(cls.__name__)
+        return [cls.get(ident) for ident in identifiers]
+
+    @classmethod
+    def clear(cls):
+        all_items = cls.all()
+        for item in all_items:
+            item.delete()
+
+    @classmethod
+    def get(cls, ident):
+        item = cls(ident)
+        if cls.exists(item):
+            item.update()
+            return item
+        else:
+            return None
+
+    @classmethod
+    def exists(cls, item):
+        return cls._redis.get(item.redis_key) is not None
+
+    def make_key(self, *args):
+        """
+        Creates a redis key from a list
+
+        Format is:
+        ClassName:identifier:attribute
+        """
+        return str.join(':', args)
+
+    def update(self):
+        """
+        Update all of the attributes from redis.
+        """
+        unsafe_attrs = set(('redis_key', 'ident', 'name'))
+        for attr, value in self.__dict__.iteritems():
+            if attr not in unsafe_attrs:
+                key = self.make_key(self.redis_key, attr)
+                self.__dict__[attr] = BaseItem._redis.get(key)
+
+    def save(self):
+        """
+        Save all of the attributes to redis
+        """
+        BaseItem._redis.sadd(self.name, self.ident)
+        BaseItem._redis.set(self.redis_key, self.object_id)
+        for attr, value in self.__dict__.iteritems():
+            key = self.make_key(self.redis_key, attr)
+            BaseItem._redis.set(key, value)
+
+    def delete(self):
+        """
+        Delete this object from redis
+        """
+        BaseItem._redis.delete(self.redis_key)
+        BaseItem._redis.srem(self.name, self.ident)
+        for attr in self.__dict__.keys():
+            key = self.make_key(self.redis_key, attr)
+            BaseItem._redis.delete(key)
+
+class Report(BaseItem):
+
+    def __init__(self, crash_id, report_dict={}):
+        self._content = None
+        self.crash_id = crash_id
+        self.timestamp = report_dict.get('timestamp', None)
+        self.content = str(report_dict)
+        super(Report, self).__init__(crash_id, crash_id)
